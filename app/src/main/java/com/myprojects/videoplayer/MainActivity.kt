2e677077ba630aa66543ae379ceacc4d5fca0854
Saifuddin <adenwala32@gmail.com>
2024-09-01 13:11:38 +0530

    Permssions and UI

--- app/src/main/java/com/myprojects/videoplayer/MainActivity.kt
+++ app/src/main/java/com/myprojects/videoplayer/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.myprojects.videoplayer
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,10 +16,36 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.myprojects.videoplayer.ui.HomeScreen
 import com.myprojects.videoplayer.ui.theme.VideoPlayerTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val permissions = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        arrayOf(
+            Manifest.permission.READ_MEDIA_VIDEO,
+            Manifest.permission.READ_MEDIA_IMAGES,
+        )
+    } else {
+        arrayOf(
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        )
+    }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        if (permissions.all { it.value }) {
+            // All permissions are granted
+            Toast.makeText(this, "Permissions granted!", Toast.LENGTH_SHORT).show()
+        } else {
+            // At least one permission is denied
+            Toast.makeText(this, "Permissions denied!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -27,5 +58,32 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+        checkAndRequestPermission()
+    }
+
+    private fun checkAndRequestPermission() {
+        when {
+            allPermissionsGranted() -> {
+                // Permissions are already granted
+                Toast.makeText(this, "Permissions already granted!", Toast.LENGTH_SHORT).show()
+            }
+            shouldShowRequestPermissionRationale() -> {
+                // Show an explanation to the user
+                Toast.makeText(this, "Permissions are required for this app.", Toast.LENGTH_SHORT).show()
+                requestPermissionLauncher.launch(permissions)
+            }
+            else -> {
+                // Directly request for required permissions
+                requestPermissionLauncher.launch(permissions)
+            }
+        }
+    }
+
+    private fun allPermissionsGranted() = permissions.all {
+        ContextCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun shouldShowRequestPermissionRationale() = permissions.any {
+        ActivityCompat.shouldShowRequestPermissionRationale(this, it)
     }
 }
--- app/src/main/java/com/myprojects/videoplayer/ui/HomeScreen.kt
+++ app/src/main/java/com/myprojects/videoplayer/ui/HomeScreen.kt
@@ -1,11 +1,55 @@
 package com.myprojects.videoplayer.ui
 
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun HomeScreen(
     modifier: Modifier
 ) {
+    var videoUri by remember { mutableStateOf<Uri?>(null) }
+
+    val videoPicker = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent(),
+        onResult = { uri ->
+            uri?.let {
+                videoUri = uri
+            }
+        }
+    )
+
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Button(
+            onClick = {
+                videoPicker.launch("video/*")
+            }
+        ) {
+            Text(
+                text = "Upload video",
+                fontSize = 15.sp,
+                color = Color.White,
+                modifier = Modifier.padding(end = 8.dp)
+            )
+        }
+    }
 
 }
